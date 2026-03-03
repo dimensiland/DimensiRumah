1c1de73746c4f3529d6bcd3b139ef7b6f67dc526
dimensiland <dimensiland.management@gmail.com>
2026-03-03 13:08:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const properties=[
 
 {
+judul:"Tanah Mewah View Sawah MURAH Hanya 17km Dari Tugu! Luas 1303m² Hanya 450rb/m! Dekat Wisata Nanggulan",
+lokasi:"Jati Sarono, Kulon Progo Regency, Special Region of Yogyakarta",
+luas:"1303m²",
+harga:"Rp 586.350.000",
+thumbnail:"Jatisarono1303.jpg",
+video:"https://youtu.be/vuW3W6CEBwQ",
+
+sertifikat:"SHM Pekarangan",
+muka:"30 meter",
+akses:"Tersedia",
+air:"Tersedia Sumur",
+listrik:"PLN tersedia",
+lingkungan:"Perkampungan, Persawahan dan Perkebunan",
+kondisi:"Datar Terasering",
+prospek:"Rumah tinggal, Rumah pensiun, Kebun, Investasi, Resto, Villa ataupun Resort.",
+point:"KTP mana saja bisa beli, Akses Mudah karena dekat dengan kantor kecamatan Nanggulan, Lingkungan perkampungan hidup dan ramai, Kawasan sekitaran area Wisata Kuliner Nanggulan, Dekat ke Exit Tol Sentolo sekitar 10 menit, Dekat ke Kota Jogja sekitar 17km, Ke Borobudur sekitar 40 menit, Bandara YIA sekitar 45 menit"
+},
+
+{
 judul:"Tanah Bonus Rumah Samping Sungai! Luas 3812m² Hanya 450 Juta! Dekat Kawasan Wisata Alam Menoreh!",
 lokasi:"Gerbosari, Kulon Progo Regency, Special Region of Yogyakarta",
 luas:"3812m²",
@@ -826,6 +845,7 @@ pagination.appendChild(btn);
 
 
 renderPage(currentPage);
+
 
 
 
