13c65dd69f0b3a1c05679d907a379a7ffb945dc7
dimensiland <dimensiland.management@gmail.com>
2026-07-26 14:56:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 const properties=[
 
 {
+judul:"TANAH MURAH DI NANGGULAN, Luas 947m² SHM Pekarangan Siap Bangun! Cuma 330jt Dekat Jalur Provinsi",
+lokasi:"Desa Lengkong, Donomulyo, Nanggulan, Kulon Progo, Yogyakarta",
+luas :"947m²",
+mdpl:"103 - 109",
+harga:"Rp 330.000.000",
+thumbnail:"Lengkong947.jpg",
+video:"https://youtu.be/RkUZgcfhEEI",
+
+sertifikat:"SHM Pekarangan",
+muka:"Sekitar 13 meter, dan menyamping ke belakang sekitar 50 meter",
+akses:"Masuk sekitar 100 meter dari jalan provinsi, lebar jalan akses utama 4 meter dan jalan samping 5 meter",
+air:"Tersedia",
+listrik:"PLN tersedia",
+lingkungan:"Sudah sangat ramai, asri, dan dekat dengan area pemukiman serta beberapa tempat strategis/wisata",
+kondisi:"Permukaan tanah sangat datar namun memiliki sedikit bagian yang berundak/terasiring di sisi samping",
+prospek:"Sangat cocok untuk dibangun homestay, rumah kedua, tempat peristirahatan, atau rumah masa pensiun",
+point:"Lokasi sangat strategis dekat wisata Nanggulan, Hanya berjarak 700 meter dari Villa The Palarenan, Hanya berjarak 1 km dari Pasar Kerambilan dan Kuliner Omah Cantrik, Berjarak sekitar 17 km dari Tugu Jogja dan 21 km dari Bandara YIA"
+},
+
+{
 judul:"Tanah Murah View Sawah Mewah Luas 8787m² di Jogja Barat – Cocok untuk Villa & Investasi!",
 lokasi:"Purwoharjo, Kulon Progo (Dekat Desa Wisata Tinalah, Samigaluh, Kulon Progo)",
 luas :"8787m²",
